69b0266220feda681080a77294e22af596268b39
SashaZhardetskaya <sasha310209@gmail.com>
2017-11-12 22:19:14 +0200

    add IgnorePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ]
 }
