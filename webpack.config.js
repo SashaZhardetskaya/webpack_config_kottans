ea4aa11b5da8e39f8020374bf827ee2d0c287125
SashaZhardetskaya <sasha310209@gmail.com>
2017-11-12 20:05:59 +0200

    init basic settings

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/feed/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'feed'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', {
+                targets: {
+                  browsers: ['last 2 versions', 'ie >= 11']
+                }
+              }]],
+              plugins: ['lodash']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
